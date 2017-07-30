0e09ba29c5bccc2cd3766481f279ce8c2e352a8c
Bruno Haible <bruno@clisp.org>
2017-07-30 09:03:51 +0200

    Move libffcall version number to a separate include file.

--- ffcall-version.in.h
+++ ffcall-version.in.h
@@ -1,8 +1,5 @@
-#ifndef _TRAMPOLINE_R_H
-#define _TRAMPOLINE_R_H
-
 /*
- * Copyright 1995-2017 Bruno Haible <bruno@clisp.org>
+ * Copyright 2009-2017 Bruno Haible <bruno@clisp.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,16 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef __cplusplus
-typedef int (*__TR_function) (...);
-#else
-typedef int (*__TR_function) ();
+#if !defined(LIBFFCALL_VERSION)
+/* Version number of libffcall: (major<<8) + minor. */
+# define LIBFFCALL_VERSION 0
 #endif
-extern __TR_function alloc_trampoline_r (__TR_function, void*, void*);
-extern void free_trampoline_r (__TR_function);
-extern int is_trampoline_r (void*);
-extern __TR_function trampoline_r_address (void*);
-extern void* trampoline_r_data0 (void*);
-extern void* trampoline_r_data1 (void*);
-
-#endif /* _TRAMPOLINE_R_H */
