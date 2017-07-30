3f14a5598f0f9547359081d48e7c742481bd822c
Bruno Haible <bruno@clisp.org>
2017-07-30 09:03:55 +0200

    Move ABI indicator macro definitions to a separate include file.

---
+++ porting-tools/abis/predefines.c
@@ -0,0 +1,160 @@
+/* Process this file with
+     ${CC} -E predefines.c | sed -e '/^#/d' -e '/^$/d'
+ */
+
+#ifdef __i386__
+HAS__i386__ => __i386__
+#endif
+#ifdef __i386
+HAS__i386 => __i386
+#endif
+#ifdef _M_IX86
+HAS_M_IX86 => _M_IX86
+#endif
+#ifdef _X86_
+HAS_X86_ => _X86_
+#endif
+#ifdef _I386
+HAS_I386 => _I386
+#endif
+#ifdef __m68k__
+HAS__m68k__ => __m68k__
+#endif
+#ifdef __mips__
+HAS__mips__ => __mips__
+#endif
+#ifdef __mips
+HAS__mips => __mips
+#endif
+#ifdef _ABIO32
+HAS_ABIO32 => _ABIO32
+#endif
+#ifdef _ABIN32
+HAS_ABIN32 => _ABIN32
+#endif
+#ifdef _MIPS_SZLONG
+HAS_MIPS_SZLONG => _MIPS_SZLONG
+#endif
+#ifdef __mips64
+HAS__mips64 => __mips64
+#endif
+#ifdef _MIPSEB
+HAS_MIPSEB => _MIPSEB
+#endif
+#ifdef _MIPSEL
+HAS_MIPSEL => _MIPSEL
+#endif
+#ifdef __sparc__
+HAS__sparc__ => __sparc__
+#endif
+#ifdef __sparc
+HAS__sparc => __sparc
+#endif
+#ifdef __sparcv9
+HAS__sparcv9 => __sparcv9
+#endif
+#ifdef __arch64__
+HAS__arch64__ => __arch64__
+#endif
+#ifdef __alpha__
+HAS__alpha__ => __alpha__
+#endif
+#ifdef __alpha
+HAS__alpha => __alpha
+#endif
+#ifdef __hppa__
+HAS__hppa__ => __hppa__
+#endif
+#ifdef __hppa
+HAS__hppa => __hppa
+#endif
+#ifdef __arm__
+HAS__arm__ => __arm__
+#endif
+#ifdef __ARM_PCS_VFP
+HAS__ARM_PCS_VFP => __ARM_PCS_VFP
+#endif
+#ifdef __ARMEL__
+HAS__ARMEL__ => __ARMEL__
+#endif
+#ifdef __ARMEB__
+HAS__ARMEB__ => __ARMEB__
+#endif
+#ifdef __aarch64__
+HAS__aarch64__ => __aarch64__
+#endif
+#ifdef __ARM_64BIT_STATE
+HAS__ARM_64BIT_STATE => __ARM_64BIT_STATE
+#endif
+#ifdef __ARM_PCS_AAPCS64
+HAS__ARM_PCS_AAPCS64 => __ARM_PCS_AAPCS64
+#endif
+#ifdef _M_ARM64
+HAS_M_ARM64 => _M_ARM64
+#endif
+#ifdef __powerpc__
+HAS__powerpc__ => __powerpc__
+#endif
+#ifdef __powerpc
+HAS__powerpc => __powerpc
+#endif
+#ifdef __ppc__
+HAS__ppc__ => __ppc__
+#endif
+#ifdef __ppc
+HAS__ppc => __ppc
+#endif
+#ifdef _IBMR2
+HAS_IBMR2 => _IBMR2
+#endif
+#ifdef _ARCH_PPC
+HAS_ARCH_PPC => _ARCH_PPC
+#endif
+#ifdef __powerpc64__
+HAS__powerpc64__ => __powerpc64__
+#endif
+#ifdef __ppc64__
+HAS__ppc64__ => __ppc64__
+#endif
+#ifdef _ARCH_PPC64
+HAS_ARCH_PPC64 => _ARCH_PPC64
+#endif
+#ifdef _CALL_ELF
+HAS_CALL_ELF => _CALL_ELF
+#endif
+#ifdef __ia64__
+HAS__ia64__ => __ia64__
+#endif
+#ifdef __ia64
+HAS__ia64 => __ia64
+#endif
+#ifdef __x86_64__
+HAS__x86_64__ => __x86_64__
+#endif
+#ifdef __amd64__
+HAS__amd64__ => __amd64__
+#endif
+#ifdef _M_X64
+HAS_M_X64 => _M_X64
+#endif
+#ifdef _M_AMD64
+HAS_M_AMD64 => _M_AMD64
+#endif
+#ifdef __s390__
+HAS__s390__ => __s390__
+#endif
+#ifdef __s390x__
+HAS__s390x__ => __s390x__
+#endif
+#ifdef _LP64
+HAS_LP64 => _LP64
+#endif
+#ifdef __LP64__
+HAS__LP64__ => __LP64__
+#endif
+#ifdef _ILP32
+HAS_ILP32 => _ILP32
+#endif
+#ifdef __ILP32__
+HAS__ILP32__ => __ILP32__
+#endif
